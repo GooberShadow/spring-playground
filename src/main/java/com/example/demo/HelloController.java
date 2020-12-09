afc86a57c5902c9225c763aeb4208e0c7091daeb
GooberShadow <benkehe@sbcglobal.net>
2020-12-09 09:19:18 -0600

    pi

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,31 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-
-    @GetMapping("/")
-    public String helloWorld() {
+    @GetMapping("/hello")
+    public String helloWorld()
+    {
         return "Hello from Spring!";
+    }
+
+    @GetMapping("/math/pi")
+    public String pi() {
+        return "3.141592653589793";
+    }
+
+
+    @GetMapping("/math/calculate")
+    public String calculate(@RequestParam Integer x, @RequestParam Integer y, @RequestParam(defaultValue = "add") String operation)
+    {
+        return MathService.calculate(operation, x, y).toString();
+    }
+
+    @PostMapping("/math/sum")
+    public String sum(@RequestParam Integer [] n)
+    {
+        return MathService.sum(n).toString();
     }
 
 }
